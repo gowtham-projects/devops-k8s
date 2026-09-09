a686e951a3c4c66eb8b12d71de4d8433c5cb042a
Gowtham Kalla <92290352+gowtham-projects@users.noreply.github.com>
2026-09-09 22:03:02 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return """
+    <html>
+        <head>
+            <title>DevOps Demo Application</title>
+        </head>
+        <body>
+            <h1>Hello from DevOps CI/CD!</h1>
+            <h2>Version: 1.0</h2>
+            <p>Environment: Development</p>
+            <h2>Successfully deployed App using Jenkins with ArgoCD </h2>
+        </body>
+    </html>
+    """
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
